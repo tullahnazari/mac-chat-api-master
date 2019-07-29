4e8a6f3b249cc31ac3d6e4782f67fbc18cd98ae2
Mr. Tee <tullah.nazari@gmail.com>
2019-07-29 06:59:00 -0400

    testing locally

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,9 @@
 import mongodb from 'mongodb';
 
 export default {
-  // "port": 3005,
-  // "mongoUrl": "mongodb://localhost:27017/chat-api",
-  "port": process.env.PORT,
-  "mongoUrl": "mongodb+srv://tullahnazari:wahdat08@cluster0-hxzfc.mongodb.net/test?retryWrites=true&w=majority",
+   "port": 3005,
+   "mongoUrl": "mongodb://localhost:27017/chat-api",
+//  "port": process.env.PORT,
+//  "mongoUrl": "mongodb+srv://tullahnazari:wahdat08@cluster0-hxzfc.mongodb.net/test?retryWrites=true&w=majority",
   "bodyLimit": "100kb"
 }
